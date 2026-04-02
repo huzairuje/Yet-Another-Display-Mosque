97aec80d03ceffd2b21f56a14b14a785cf6c0234
huzairuje <muhammadhuzair@gmail.com>
2026-04-03 00:35:12 +0700

    remove binary server

--- linux/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PARENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-cd "$PARENT_PATH"
-echo "🚀 Menjalankan YADM dari folder runner..."
-export PORT=3000
-export HOST=0.0.0.0
-node build
